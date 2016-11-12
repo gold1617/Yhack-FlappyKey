9344d721ae29d27e608aa250ef4cf3b8aca3da7b
Rishab Chhabra <Rishab Chhabra>
2016-11-12 10:20:49 -0500

    Implemented user input to move bird

--- Flappy Key/Flappy_Key.cpp
+++ Flappy Key/Flappy_Key.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <thread>
 #include <string>
+#include <Windows.h>
 
 
 const char* toString(CorsairError error)
@@ -44,7 +45,29 @@ int main()
 		std::this_thread::sleep_for(std::chrono::seconds(1));
 		bird = CorsairLedColor{ bird_loc,0,0,0 };
 		CorsairSetLedsColors(1, &bird);
-		bird_loc = static_cast<CorsairLedId>(bird_loc-1);
+
+		if (GetAsyncKeyState(VK_ESCAPE))//If user presses ESC bird moves up
+		{
+			if (bird_loc < CLK_F11)
+			{
+				bird_loc = static_cast<CorsairLedId>(bird_loc + 1);
+			}
+			else if (bird_loc == CLK_F11)
+			{
+				bird_loc = static_cast<CorsairLedId>(CLK_F12);
+			}
+		}
+		else
+		{
+			if(bird_loc == CLK_F12)
+			{
+				bird_loc = static_cast<CorsairLedId>(CLK_F11);
+			}
+			else
+			{
+				bird_loc = static_cast<CorsairLedId>(bird_loc - 1);
+			}
+		}
 	}
 
 	std::cout << "bird died :/";
