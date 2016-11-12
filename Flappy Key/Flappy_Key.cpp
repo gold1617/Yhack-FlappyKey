9defa77ae038e51df3e2e00f78cbf4467502b02a
Rishab Chhabra <Rishab Chhabra>
2016-11-12 09:40:12 -0500

    Starting to play with 'moving pixels'

---
+++ Flappy Key/Flappy_Key.cpp
@@ -0,0 +1,54 @@
+
+#include "CUESDK.h"
+
+#include <iostream>
+#include <thread>
+#include <string>
+
+
+const char* toString(CorsairError error)
+{
+	switch (error) {
+	case CE_Success:
+		return "CE_Success";
+	case CE_ServerNotFound:
+		return "CE_ServerNotFound";
+	case CE_NoControl:
+		return "CE_NoControl";
+	case CE_ProtocolHandshakeMissing:
+		return "CE_ProtocolHandshakeMissing";
+	case CE_IncompatibleProtocol:
+		return "CE_IncompatibleProtocol";
+	case CE_InvalidArguments:
+		return "CE_InvalidArguments";
+	default:
+		return "unknown error";
+	}
+}
+
+int main()
+{
+	CorsairPerformProtocolHandshake();
+
+	if (const auto error = CorsairGetLastError()) {
+		std::cout << "Handshake failed: " << toString(error) << std::endl;
+		getchar();
+		return -1;
+	}
+	auto bird_loc = CLK_F6;//Start "bird" at F6 key
+	CorsairRequestControl(CAM_ExclusiveLightingControl);
+	while (bird_loc != CLK_Escape)
+	{
+		auto bird = CorsairLedColor{ bird_loc,255,0,0 };
+		CorsairSetLedsColors(1, &bird);
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		bird = CorsairLedColor{ bird_loc,0,0,0 };
+		CorsairSetLedsColors(1, &bird);
+		bird_loc = static_cast<CorsairLedId>(bird_loc-1);
+	}
+
+	std::cout << "bird died :/";
+	getchar();
+
+	return 0;
+}
